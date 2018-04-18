46f948216961743e34d00497de2c63a35c9d054c
Lorderot <Ihorkayf_@mail.ru>
2018-04-18 03:23:16 +0300

    removed

--- TurfClubScrapper/TurfClubScrapper.Models/Enums/ApplicationMode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TurfClubScrapper.Models.Enums
-{
-    public enum ApplicationMode
-    {
-        PastResultsMode = 1000,
-        DailyMode = 1001,
-        UpcomingRacesMode = 1002
-    }
-}
